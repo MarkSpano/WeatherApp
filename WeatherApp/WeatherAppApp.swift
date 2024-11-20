8328c2583a235720c080e7f2c62b998c99323ea3
Mark Spano <mspano@glysens.com>
2024-11-20 16:09:45 -0700

    Created Xcode project

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Mark Spano on 11/20/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
